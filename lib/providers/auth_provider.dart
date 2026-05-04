8c24a566d88d9e2253be5c00406fc676a8c6630f
MphephuSamuel <mphephusamuel17@gmail.com>
2026-05-04 04:54:27 +0200

    authorization

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
+import 'dart:developer';
 
 class AuthProvider with ChangeNotifier {
   final AuthService _authService = AuthService();
@@ -14,11 +15,15 @@ class AuthProvider with ChangeNotifier {
       final user = await _authService.signIn(email, password);
       if (user != null) {
         _token = await _authService.getToken();
+        log('User logged in', name: 'auth');
         _isAuthenticated = true;
         notifyListeners();
       }
     } catch (e) {
-      rethrow;
+      log('Login failed: $e', name: 'auth', error: e);
+      throw Exception(
+        'Login failed. Please check your credentials and try again.',
+      );
     }
   }
 
@@ -31,6 +36,7 @@ class AuthProvider with ChangeNotifier {
 
   Future<void> loadToken() async {
     _token = await _authService.getToken();
+    debugPrint('Loaded Token: $_token'); // Debug print to confirm token loading
     _isAuthenticated = _token != null;
     notifyListeners();
   }
